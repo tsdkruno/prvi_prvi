9466ce468eb2bcbba447268fc824b1b5cfba55fb
Krunoslav Husak <krunoslav.husak@skole.hr>
2016-11-24 11:29:35 +0100

    Prvi unos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prvi</title>
+    </head>
+
+    <body>
+        <h1>Prvi PHP</h1>
+        <?php echo date('d.m.Y. H:i:s'); ?>
+    </body>
+</html>
